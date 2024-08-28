955953fe2662e263852889a64bb164fc8f764eb6
Kaio Henrique <paulwallkeruchiha@gmail.com>
2024-08-28 18:56:29 -0300

    laço de repetição

---
+++ aula 03/para2.py
@@ -0,0 +1,18 @@
+alunos = [
+    "Ney",
+    "Neymar",
+    "Neymar jr",
+    "Menino Ney",
+    "Menino da villa",
+    "Neymar junior",
+    "Neymar junior do santos",
+    "Homi Ney",
+    "Neymar jr every bare",
+]
+alunos.sort()
+alunos.append("Menino Ney")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+x = y +1
